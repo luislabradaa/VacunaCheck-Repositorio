8db90409681777dc02967590edc20a1f2c143c24
lizbeth perez salinas <liz.p.salinas23@gmail.com>
2022-04-20 16:42:43 -0500

    cambios heroku

--- controllers/scannController.js
+++ controllers/scannController.js
@@ -1,22 +1,7 @@
-<<<<<<< HEAD
-
-const express = require('express');
-const app = express();
-var User = require('../models/user');
-var { body,validationResult } = require('express-validator');
-const bcrypt = require("bcryptjs");
-const src = require('debug');
-const QRCode = require('qrcode');
-
 
 exports.verifyCode = (function(req, res) {
     
    res.json({ vacunado: 'Estas vacunado' });
 
-=======
-
-exports.verifyCode = (function(req, res) {
-    res.json({vacunado: 'Estas vacunado' });
->>>>>>> 7248a69177a25727604d3a53985b1e522efe2400
 
 });
