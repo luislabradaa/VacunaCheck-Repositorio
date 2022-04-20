a6634ece2546fe4dee6b64948eb0eb951f62b2b2
lizbeth perez salinas <liz.p.salinas23@gmail.com>
2022-04-20 13:12:23 -0500

    cambios registro

--- controllers/scannController.js
+++ controllers/scannController.js
@@ -1,4 +1,4 @@
-<<<<<<< HEAD
+
 const express = require('express');
 const app = express();
 var User = require('../models/user');
@@ -12,8 +12,5 @@ exports.verifyCode = (function(req, res) {
     
    res.json({ vacunado: 'Estas vacunado' });
 
-=======
-exports.verifyCode = (function(req, res) {
-    res.json({vacunado: 'Estas vacunado' });
->>>>>>> c67741df6e81ea1155edd87311afc8da4cb64c7d
+
 });
